39e2f15644e5e1f87d6cafe8c98473d9c05f9d90
wolyshaw <shaw@xwlong.com>
2015-07-25 12:37:28 +0800

    添加cookie纪录
    不足：不能排序问题

--- mail.js
+++ mail.js
@@ -4,8 +4,58 @@ window.onload = function(){
 	var tmBut = document.getElementById('tmBut');
 	var oBut = document.getElementById('oBut');
 	var ul = document.getElementById('ul');
+	var div = document.getElementById('div');
 	var form = document.getElementById('form');
 	var state = document.getElementById('state');
+
+	function setCookie (cooName,cooVal,cooDate){
+
+		var oDate = new Date();
+		oDate.setDate(oDate.getDate()+cooDate);
+
+		var nowCoo = getCookie(cooName).split(',');
+		
+		// if(nowCoo.length <= 1){
+			
+		// 	alert(3)
+		// 	document.cookie = cooName+'='+cooVal+';expires='+oDate;
+		// 	} else if(nowCoo.length >1){
+		// 		for(var i = 1;i < nowCoo.length;i++){
+		// 			if (cooVal == nowCoo[i]) {
+		// 				alert(2)
+		// 				break;
+		// 			} else{	
+		// 				alert(1)
+		// 				var newCooVal = cooVal+','+getCookie(cooName)
+		// 				document.cookie = cooName+'='+newCooVal+';expires='+oDate;
+		// 			};
+		// 		}
+		// 	}
+		//通过search方法查询是否有相同的记录存在，如果是－1或0说明没有，如果存在会返回正数
+		if (getCookie(cooName).search(cooVal) <= 0) {
+			var newCooVal = getCookie(cooName)+','+cooVal
+			document.cookie = cooName+'='+newCooVal+';expires='+oDate;
+			
+		} else{
+			return false;
+		};
+
+	};
+
+	function getCookie(cooName){
+		var cooArr = document.cookie.split('; ');
+
+		for(var i = 0;i < cooArr.length;i++){
+			var arrName = cooArr[i].split('=');
+			if (arrName[0] == cooName) {
+				return arrName[1];
+			};
+		};
+
+		return '';
+	};
+
+
 
 	tmBut.onclick = function(){
 
@@ -20,6 +70,8 @@ window.onload = function(){
 			else
 			{
 				li.innerHTML='<a data-type="0" biz-itemid="'+id+'" data-tmpl="628x100" data-tmplid="7" data-rd="2" data-style="2" data-border="1" href=http://item.taobao.com/item.htm?id='+id+' target="_blank">'+id+'</a>';
+
+				setCookie('tm',id,3);
 			};
 		
 		ul.appendChild(li);
@@ -85,5 +137,14 @@ window.onload = function(){
 			if (state.style.opacity==0) {state.style.display = 'none'};
 		},30);
 	};
+
+		var oArr = getCookie('tm').split(',');
+
+		for(var i = 1;i < oArr.length; i++){
+			var span = document.createElement('span');
+			span.innerHTML='<a data-type="0" biz-itemid="'+oArr[i]+'" data-tmpl="628x100" data-tmplid="7" data-rd="2" data-style="2" data-border="1" href=http://item.taobao.com/item.htm?id='+oArr[i]+' target="_blank">'+oArr[i]+'</a>';
+			div.appendChild(span);
+		}
+
 	console.log('shaw@xwlong.com')
 };
